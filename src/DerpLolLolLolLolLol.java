c69e63734bbb2cbb87673dec6038e61339173c83
quentin <qcornevin@gmail.com>
2016-01-10 15:32:31 +0100

    init

---
+++ src/DerpLolLolLolLolLol.java
@@ -0,0 +1,11 @@
+import org.lucci.lmu.gui.LMUGui;
+
+/**
+ * Created by quentin on 15/12/15.
+ */
+public class DerpLolLolLolLolLol {
+
+    public static void main(String argc[]) {
+        new LMUGui("load src/yolo");
+    }
+}
