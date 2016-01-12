629b1134d3d853ea1ecc0bcde669c8360c6a2987
Tom GUILLERMIN <tom.guillermin@gmail.com>
2016-01-12 09:01:07 +0100

    Added LMU-Eclipse-UI plugin

---
+++ plugin/fr.unice.polytech.rimel.LMUEclipseUI/src/fr/unice/polytech/rimel/testplugin/ContextMenuHandler.java
@@ -0,0 +1,38 @@
+package fr.unice.polytech.rimel.testplugin;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+
+public class ContextMenuHandler implements IHandler {
+
+	@Override
+	public void addHandlerListener(IHandlerListener handlerListener) {
+	}
+
+	@Override
+	public void dispose() {	
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		System.out.println("MEGA LOL");
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean isHandled() {
+		return true;
+	}
+
+	@Override
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+	}
+
+}
