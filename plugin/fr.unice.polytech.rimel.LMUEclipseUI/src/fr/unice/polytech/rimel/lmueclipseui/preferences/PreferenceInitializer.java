8a209918bf41b80be54700692c6396fb5dffb563
Tom GUILLERMIN <tom.guillermin@gmail.com>
2016-01-12 10:53:46 +0100

    Added Doc plugin and preference page.

---
+++ plugin/fr.unice.polytech.rimel.LMUEclipseUI/src/fr/unice/polytech/rimel/lmueclipseui/preferences/PreferenceInitializer.java
@@ -0,0 +1,26 @@
+package fr.unice.polytech.rimel.lmueclipseui.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+import fr.unice.polytech.rimel.testplugin.Activator;
+
+/**
+ * Class used to initialize default preference values.
+ */
+public class PreferenceInitializer extends AbstractPreferenceInitializer {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
+	 */
+	public void initializeDefaultPreferences() {
+		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+		store.setDefault(PreferenceConstants.P_BOOLEAN, true);
+		store.setDefault(PreferenceConstants.P_CHOICE, "choice2");
+		store.setDefault(PreferenceConstants.P_STRING,
+				"Default value");
+	}
+
+}
