4e7f34f47871542bcf476851e5a6b92e8cba4cf4
Rémy <ptitdup08@hotmail.fr>
2016-02-18 15:49:43 +0100

    my changes from years

--- core/src/main/java/org/lucci/lmu/test/ReadManifestTest.java
+++ core/src/main/java/org/lucci/lmu/test/ReadManifestTest.java
@@ -1,7 +1,35 @@
 package org.lucci.lmu.test;
 
+import toools.io.file.JarFile;
+
+import java.io.File;
+
+
 /**
  * Created by quentin on 09/02/16.
  */
 public class ReadManifestTest {
+    private String jarName = "lmu-eclipse-plugin-bkp_1.0.0.201601261055.jar";
+    private String jarPath;
+    private JarFile jarFile;
+
+    public void setJarPath(){
+        this.jarPath = "/home/remy/Documents/RIMEL/projet/resources/"+jarName;
+    }
+
+    public JarFile createJarFile(){
+        return jarFile = new JarFile(jarPath);
+    }
+
+    public static void main(String[] args) {
+        ReadManifestTest readManifestTest = new ReadManifestTest();
+        readManifestTest.setJarPath();
+        JarFile jarFile = readManifestTest.createJarFile();
+
+        
+
+        // get Manifest here
+
+        System.out.println(jarFile);
+    }
 }
