94d965ae8ffb5e76c7330a10af6a7ac5f4b3fb5f
quentin <qcornevin@gmail.com>
2016-02-19 13:52:42 +0100

    clean

--- core/src/main/java/org/lucci/lmu/test/ReadManifestTest.java
+++ core/src/main/java/org/lucci/lmu/test/ReadManifestTest.java
@@ -13,18 +13,6 @@ import java.io.IOException;
 public class ReadManifestTest {
 
     public static void main(String argc[]) throws WriterException, IOException {
-    //    DeploymentUnitAnalyzer deploymentUnitAnalyzer = new DeploymentUnitAnalyzer();
-    //    AbstractModel abstractModel = deploymentUnitAnalyzer.createModelFromJar("/home/quentin/Documents/SI5/Retro/RetroEngineering/core/src/main/resources/lmu-eclipse-plugin-bkp_1.0.0.201601261055.jar");
-
-   //     PluginAnalyser pluginAnalyser = new PluginAnalyser();
-   //     AbstractModel abstractModel = pluginAnalyser.createModelFromPlugin(Directory.getCurrentDirectory().getPath()+"/../plugin/fr.unice.polytech.rimel.LMUEclipseUI");
-     //   Path outputPath = Paths.get("/home/quentin/Documents/SI5/Retro/RetroEngineering/core/test.pdf");
-
-
-       // AbstractWriter writer = WriterFactory.getTextFactory("pdf");
-       // FileUtilities.setFileContent(outputPath.toFile(), writer.writeModel(abstractModel));
-
-
         LmuConfiguration lmuConfiguration = new LmuConfiguration();
         lmuConfiguration.setJarUnitPath("/home/quentin/Documents/SI5/Retro/RetroEngineering/core/src/main/resources/lmu-eclipse-plugin-bkp_1.0.0.201601261055.jar");
         lmuConfiguration.setOuputFileName("test");
