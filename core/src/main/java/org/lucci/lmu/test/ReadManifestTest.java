726bb660b0111578190f4f9b06034f216cef0e7e
quentin <qcornevin@gmail.com>
2016-02-19 13:52:15 +0100

    API OK

--- core/src/main/java/org/lucci/lmu/test/ReadManifestTest.java
+++ core/src/main/java/org/lucci/lmu/test/ReadManifestTest.java
@@ -1,15 +1,11 @@
 package org.lucci.lmu.test;
 
-import org.lucci.lmu.domain.AbstractModel;
-import org.lucci.lmu.input.DeploymentUnitAnalyzer;
-import org.lucci.lmu.output.AbstractWriter;
 import org.lucci.lmu.output.WriterException;
-import org.lucci.lmu.output.WriterFactory;
-import toools.io.FileUtilities;
+import org.lucci.lmu.plugin_communication.ConfigurationException;
+import org.lucci.lmu.plugin_communication.LmuConfiguration;
+import org.lucci.lmu.plugin_communication.OutputAvailable;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  * Created by quentin on 09/02/16.
@@ -17,16 +13,28 @@ import java.nio.file.Paths;
 public class ReadManifestTest {
 
     public static void main(String argc[]) throws WriterException, IOException {
-        DeploymentUnitAnalyzer deploymentUnitAnalyzer = new DeploymentUnitAnalyzer();
-        AbstractModel abstractModel = deploymentUnitAnalyzer.createModelFromJar("/home/quentin/Documents/SI5/Retro/RetroEngineering/core/src/main/resources/lmu-eclipse-plugin-bkp_1.0.0.201601261055.jar");
+    //    DeploymentUnitAnalyzer deploymentUnitAnalyzer = new DeploymentUnitAnalyzer();
+    //    AbstractModel abstractModel = deploymentUnitAnalyzer.createModelFromJar("/home/quentin/Documents/SI5/Retro/RetroEngineering/core/src/main/resources/lmu-eclipse-plugin-bkp_1.0.0.201601261055.jar");
 
    //     PluginAnalyser pluginAnalyser = new PluginAnalyser();
    //     AbstractModel abstractModel = pluginAnalyser.createModelFromPlugin(Directory.getCurrentDirectory().getPath()+"/../plugin/fr.unice.polytech.rimel.LMUEclipseUI");
-        Path outputPath = Paths.get("/home/quentin/Documents/SI5/Retro/RetroEngineering/core/test.pdf");
+     //   Path outputPath = Paths.get("/home/quentin/Documents/SI5/Retro/RetroEngineering/core/test.pdf");
 
 
-        AbstractWriter writer = WriterFactory.getTextFactory("pdf");
-        FileUtilities.setFileContent(outputPath.toFile(), writer.writeModel(abstractModel));
+       // AbstractWriter writer = WriterFactory.getTextFactory("pdf");
+       // FileUtilities.setFileContent(outputPath.toFile(), writer.writeModel(abstractModel));
+
+
+        LmuConfiguration lmuConfiguration = new LmuConfiguration();
+        lmuConfiguration.setJarUnitPath("/home/quentin/Documents/SI5/Retro/RetroEngineering/core/src/main/resources/lmu-eclipse-plugin-bkp_1.0.0.201601261055.jar");
+        lmuConfiguration.setOuputFileName("test");
+        lmuConfiguration.setOutputExtension(OutputAvailable.PDF);
+        try {
+            lmuConfiguration.createModel();
+        } catch (ConfigurationException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Work is done !!");
     }
 }
